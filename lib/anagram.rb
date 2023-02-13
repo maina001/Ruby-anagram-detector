0272b20126ab0e1dfb29953417ed09dcc55823f2
Brian <brianmakorious@gmail.com>
2023-02-13 13:06:32 +0300

    done with array

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,3 +5,12 @@ class Anagram
     def initialize(word)
       @word = word
     end
+
+    def match(array)
+        array.select do |arr|
+          arr.split("").sort == @word.split("").sort
+        end
+      end
+    
+    
+    end
